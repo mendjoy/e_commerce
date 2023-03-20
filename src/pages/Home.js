e49d77712c66a6d5edf277658b2f8b71d40ed765
Joyce Mendes <joycemendes.jm79@gmail.com>
2023-03-20 18:28:09 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,35 @@ import { useState, useEffect } from 'react'
 //api
 import { getJewelery } from '../services/jeweleryService'
 
+//css
+import './Home.css'
+
 const Home = () => {
 
-    const [jeweleryData, setJeweleryData] = useState([]);
-    console.log(jeweleryData)
-
-    useEffect(() => {
-        const fetchJewelery = async () => {
-          const data = await getJewelery();
-          setJeweleryData(data);
-        };
-        fetchJewelery();
-      }, []);
+  const [jeweleryData, setJeweleryData] = useState([]);
+  console.log(jeweleryData)
+  
+  useEffect(() => {
+      const fetchJewelery = async () => {
+        const data = await getJewelery();
+        setJeweleryData(data.products);
+      };
+      fetchJewelery();
+    }, []);
 
   return (
-    <div>
+    <div className='product-container'>
+
+     <div className='banner'></div>
+
+      { jeweleryData.length === 0 && <p>Carregando...</p>}
+      { jeweleryData.map((product) => 
+      <div key={product.id} className='product-info'>
+        <img src={product.thumbnail} alt={product.title}/>
+        <h3>{product.title}</h3>
+        <p>{product.price}</p>
+      </div>)}
+      
       
 
     </div>
