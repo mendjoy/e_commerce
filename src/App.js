ee9804d22a5ae36a7b17cff121fc063d8086836d
Joyce Mendes <joycemendes.jm79@gmail.com>
2023-03-21 14:35:48 -0300

    form de login e register

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 //pages
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 //components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
 
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
           </Routes> 
         </div>
         <Footer/>
---
+++ src/pages/Register.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+//icons
+import { FiUserPlus } from 'react-icons/fi'
+
+//css 
+import './LoginAndRegister.css'
+
+const Register = () => {
+  return (
+    <div className='register-container'>
+        <div className='register-area'>
+            <h4><FiUserPlus size={28} color='#2B2B2B'/> Register</h4>
+            <form className='form'>
+                <input type="text" placeholder='Name' />
+                <input type="text" placeholder='Lastname' />
+                <input type="email" placeholder='E-mail'/>
+                <input type="password" placeholder='Password' />
+                <input type="password" placeholder='Confirm password'/>
+
+                <div className='button-container'>
+                    <input className='sign-btn'  type="submit" value='Sign Up' />
+                </div>
+            </form>
+            <p>Does not have an account? <Link to='/login'>Login</Link></p>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Register
