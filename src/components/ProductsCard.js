3be569cd94745262be818afe2e00bd7a9da355e9
Joyce Mendes <joycemendes.jm79@gmail.com>
2023-03-22 21:53:19 -0300

    adicionando rotas para informações do item

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -2,33 +2,46 @@ import React from 'react'
 //icons
 import { FiHeart, FiShoppingCart } from 'react-icons/fi'
 import { VscStarHalf } from 'react-icons/vsc'
+import { Link, useNavigate } from 'react-router-dom'
 
 //css
 import './ProductsCard.css'
 
 const ProductsCard = ({product}) => {
+
+    const navigate = useNavigate();
+
+    const handleLinkClick = async (event) => {
+        event.preventDefault();
+        await navigate('/');
+        setTimeout(() => {
+          navigate(`/product/${product.id}`);
+        }, 100);
+      };
+
   return (
     <div className='product-card-container'>
         <div key={product.id} className='product-info'>
-            <img src={product.thumbnail} alt={product.title}/>
-            <h3>{product.title}</h3>
-            <p>R$ {product.price},00</p> 
+            <Link onClick={handleLinkClick}>
+                <img src={product.thumbnail} alt={product.title}/>
+                <h3>{product.title}</h3>
+                <p>R$ {product.price},00</p> 
+            </Link>
+                <div className='icons'>
+                    <div className='rating'>
+                        <VscStarHalf size={23} color='#eca400'/>{product.rating}
+                    </div>
+                    <div className='button-container'>
+                        <button>
+                            <FiHeart size={23} color='#2B2B2B'/>
+                        </button>
 
-            <div className='icons'>
-                <div className='rating'>
-                    <VscStarHalf size={23} color='#eca400'/>{product.rating}
+                        <button>
+                            <FiShoppingCart size={23} color='#2B2B2B'/>
+                        </button>
+                    </div>  
                 </div>
-                <div className='button-container'>
-                    <button>
-                        <FiHeart size={23} color='#2B2B2B'/>
-                    </button>
-
-                    <button>
-                        <FiShoppingCart size={23} color='#2B2B2B'/>
-                    </button>
-              </div>  
-            </div>
-      </div>
+        </div>
     </div>
   )
 }
