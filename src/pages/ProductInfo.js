603be5eed8b778e140087a516c448c27d5abf019
Joyce Mendes <joycemendes.jm79@gmail.com>
2023-03-23 20:16:17 -0300

    pagina de especificações do produto

---
+++ src/pages/ProductInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//api 
+import { useFetchProductById } from '../hooks/useFetchProductById'
+
+const ProductInfo = () => {
+    const { productInfo } = useFetchProductById()
+    console.log(productInfo)
+   
+  return (
+    <div>
+        {productInfo == null && <p>Aguarde...</p>}
+        {productInfo != null && 
+        <div>
+            <h3>{productInfo.title}</h3>
+            <img src={productInfo.thumbnail} alt={productInfo.title} />
+            <p>Brand: {productInfo.brand}</p>
+            <p>{productInfo.description}</p>
+            <p>price:{productInfo.price}</p>
+            <p>{productInfo.discountPercentage}</p>
+            <p>stock: {productInfo.stock}</p>
+        </div>}
+     
+    </div>
+  )
+}
+
+export default ProductInfo
